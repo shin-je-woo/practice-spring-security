b621e1aca8f4d3cfa8afc330fdae95ce67d72860
shin.jewoo <shinjw0926@naver.com>
2023-10-10 22:09:41 +0900

    [Section2] Authentication, SecurityContextHolder, SecurityContext

--- src/main/java/io/security/basicsecurity/SecurityController.java
+++ src/main/java/io/security/basicsecurity/SecurityController.java
@@ -1,5 +1,10 @@
 package io.security.basicsecurity;
 
+import jakarta.servlet.http.HttpSession;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +12,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class SecurityController {
 
     @GetMapping("/")
-    public String index() {
+    public String index(HttpSession session) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        SecurityContext securityContext = (SecurityContext) session.getAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY);
+        Authentication authentication1 = securityContext.getAuthentication();
+
         return "home";
+    }
+
+    @GetMapping("/thread")
+    public String thread() {
+
+        new Thread(() -> {
+            // SecurityContextHolder의 기본 모드는 MODE_THREADLOCAL이기 때문에 쓰레드간 SecurityContext를 공유하지 않음
+            // 따라서 아래 authentication는 null이 나옴
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        }).start();
+
+        return "thread";
     }
 
     @GetMapping("/user")
