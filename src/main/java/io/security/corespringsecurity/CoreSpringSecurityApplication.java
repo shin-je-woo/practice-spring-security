fa606be484c3fdf202fa774719a46be033ed9931
shin.jewoo <shinjw0926@naver.com>
2023-10-31 22:56:30 +0900

    프로젝트 정리(초기화)

--- src/main/java/io/security/corespringsecurity/CoreSpringSecurityApplication.java
+++ src/main/java/io/security/corespringsecurity/CoreSpringSecurityApplication.java
@@ -1,13 +1,13 @@
-package io.security.basicsecurity;
+package io.security.corespringsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BasicsecurityApplication {
+public class CoreSpringSecurityApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BasicsecurityApplication.class, args);
+		SpringApplication.run(CoreSpringSecurityApplication.class, args);
 	}
 
 }
--- src/test/java/io/security/corespringsecurity/CoreSpringSecurityApplicationTests.java
+++ src/test/java/io/security/corespringsecurity/CoreSpringSecurityApplicationTests.java
@@ -1,10 +1,10 @@
-package io.security.basicsecurity;
+package io.security.corespringsecurity;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BasicsecurityApplicationTests {
+class CoreSpringSecurityApplicationTests {
 
 	@Test
 	void contextLoads() {
