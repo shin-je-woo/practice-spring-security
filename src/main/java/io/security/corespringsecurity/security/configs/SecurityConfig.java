f8fbc114227c400be22126abd6cac0870f5769d5
shin.jewoo <shinjw0926@naver.com>
2023-11-03 23:56:28 +0900

    [세팅] 화면세팅 및 시큐리티 기본 설정

---
+++ src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
@@ -0,0 +1,63 @@
+package io.security.corespringsecurity.security.configs;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@Slf4j
+public class SecurityConfig {
+
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        String password = passwordEncoder().encode("1111");
+        UserDetails user = User.builder()
+                .username("user")
+                .password(password)
+                .roles("USER")
+                .build();
+        UserDetails manager = User.builder()
+                .username("manager")
+                .password(password)
+                .roles("USER", "MANAGER")
+                .build();
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(password)
+                .roles("USER", "MANAGER", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, manager, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/").permitAll()
+                        .requestMatchers("/mypage").hasRole("USER")
+                        .requestMatchers("/messages").hasRole("MANAGER")
+                        .requestMatchers("/config").hasRole("ADMIN")
+                        .anyRequest().authenticated());
+        http
+                .formLogin(Customizer.withDefaults());
+
+        return http.getOrBuild();
+    }
+}
