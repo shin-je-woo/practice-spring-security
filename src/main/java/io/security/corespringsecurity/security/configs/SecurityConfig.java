d1405dbeec9f377cfa23a6123c116115540c61aa
shin.jewoo <shinjw0926@naver.com>
2023-12-04 23:44:08 +0900

    [메서드 인가] 어노테이션 권한 설정 - @PreAuthorize

--- src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
+++ src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
@@ -24,6 +24,7 @@ import org.springframework.security.authentication.AuthenticationDetailsSource;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -48,6 +49,7 @@ import java.util.Map;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
